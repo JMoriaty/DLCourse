2ef0b613916d8f4456cbd0f4f299c4308067ada5
Ginger <jiangzai58@gmail.com>
2024-04-06 16:53:44 +0800

    more function

--- Assignment1/my_code.py
+++ Assignment1/my_code.py
@@ -1,9 +1,15 @@
+import time
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+import scipy
+from scipy import ndimage
+from PIL import Image
 from dnn_app_utils_v2 import *
-
 
 # plt.rcParams['figure.figsize'] = (5.0, 4.0)
 # plt.rcParams['image.interpolation'] = 'nearest'
-# plt.rcParams['iamge.cmap'] = 'gray'
+# plt.rcParams['image.cmap'] = 'gray'
 
 np.random.seed(1)
 
@@ -39,6 +45,65 @@ def StandardizeImage():
 
     print("train_x's shape " + str(train_x.shape))
     print("test_x's shape " + str(test_x.shape))
+
+
+def initialize_parameters_deep(layer_dims):
+    parameters = {}
+    L = len(layer_dims)
+
+    for i in range(1, L):
+        parameters['W' + str(i)] = np.random.randn(layer_dims[i], layer_dims[1-i]) * 0.01
+        parameters['b' + str(i)] = np.random.randn(layer_dims[1], 1)
+        assert(parameters['W' + str(i)].shape == (layer_dims[1], layer_dims[i-1]))
+        assert(parameters['b' + str(i)].shape == (layer_dims[1], 1))
+
+    return parameters
+
+
+def activation_function(A_prev, W, b, activation):
+    if activation == 'sigmoid':
+        Z = np.dot(W, A_prev) + b
+        linear_cache = (A_prev, W, b)
+        A, activation_cache = sigmoid(Z)
+
+    elif activation == 'relu':
+        Z = np.dot(W, A_prev) + b
+        linear_cache = (A_prev, W, b)
+        A, activation_cache = relu(Z)
+
+    cache = (linear_cache, activation_cache)
+    return A, cache
+
+
+def L_model_forward(X, parameters):
+    caches = []
+    A = X
+    L = len(parameters) // 2
+
+    for i in range(L):
+        A_prev = A
+        A, caches = activation_function(A_prev, parameters['W' + str(i)], parameters['b' + str(i)], activation='relu')
+        caches.append(caches)
+
+    AL, caches = activation_function(A, parameters['W' + str(L)], parameters['b' + str(L)], activation='relu')
+    caches.append(caches)
+
+    return AL, caches
+
+
+def computer_cost(AL, Y):
+    m = Y.shape[1]
+    cost = (-1 / m) * np.sum(Y * np.log(AL) + (1 - AL) * np.log(1 - AL))
+    cost = np.squeeze(cost, axis=0)
+    assert(cost.shape == ())
+
+    return cost
+
+
+def L_model_backward(AL, Y, caches):
+
+
+def update_parameters(parameters, grads,learning_rate):
 
 
 if __name__ == '__main__':
